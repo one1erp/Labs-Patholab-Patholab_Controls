ddd951f3d98f26886276c9aa509afd3b68241a68
one1erp <avigaile@one1.co.il>
2024-04-08 17:27:37 +0300

    Initial commit with additional files

---
+++ Patholab_Controls/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+public static class CustomMessageBox
+{
+    public static void Show(string text, MessageBoxButtons messageBoxButtons = MessageBoxButtons.OK, MessageBoxIcon messageBoxIcon = MessageBoxIcon.None)
+    {
+        MessageBox.Show(text, "Nautilus", messageBoxButtons, messageBoxIcon, MessageBoxDefaultButton.Button1, MessageBoxOptions.RtlReading);
+    }
+}
